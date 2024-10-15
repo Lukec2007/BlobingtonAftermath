ce33475f18d30fdf99665a62c82dfc41779147de
Lukec2007 <146829004+Lukec2007@users.noreply.github.com>
2024-10-15 16:16:33 +0100

    ass

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Stats")]
+
     [SerializeField] int maxHealth = 100;
     [SerializeField] float speed = 1f;
+
+    [Header("Charger")]
+
+    [SerializeField] bool isCharger;
+
+    [SerializeField] float distanceToCharge = 5f;
+    [SerializeField] float chargeSpeed = 12f;
+    [SerializeField] float prepareTime = 2f;
+
+    bool isCharging = false;
+    bool isPreparingCharge = false;
+
 
     private int currentHealth;
 
@@ -20,6 +34,9 @@ public class Enemy : MonoBehaviour
     }
     void Update()
     {
+        if (!WaveManager.Instance.WaveRunning()) return;
+        if (isPreparingCharge) return;
+        
         if (target != null)
         {
             Vector3 direction = target.position - transform.position;
@@ -29,7 +46,23 @@ public class Enemy : MonoBehaviour
 
             var playerToTheRight = target.position.x > transform.position.x;
             transform.localScale = new Vector2(playerToTheRight ? -1 : 1, 1);
+
+            if (isCharger &&
+                !isCharging &&
+                Vector2.Distance(transform.position, target.position) < distanceToCharge)
+            {
+                isPreparingCharge = true;
+                Invoke("StartCharing", prepareTime);
+            }
         }
+
+    }
+
+      void StartCharging()
+    {
+        isPreparingCharge = false;
+        isCharging = true;
+        speed = chargeSpeed;
     }
     
         public void Hit(int damage)
